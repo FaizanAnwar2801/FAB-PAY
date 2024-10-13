d4668d9e71d3fdfbd2b230a452ba2fb4cfa5be25
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-10-13 23:32:14 +0530

    Landing page(User.)

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Appbar } from "@repo/ui/appbar";
+
+export default function Page(): JSX.Element {
+  const session = useSession();
   return (
-    <div className="text-2xl">
-      Hello User.
+    <div>
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
     </div>
   );
 }
