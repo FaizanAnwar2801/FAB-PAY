d9af35ec84b8ab7bf6d253a5a03b4c1b0ab4d445
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-10-23 02:17:18 +0530

    Modified p2p Transfer to show pop-up.

--- apps/user-app/app/(dashboard)/p2pTransfer/page.tsx
+++ apps/user-app/app/(dashboard)/p2pTransfer/page.tsx
@@ -1,5 +1,6 @@
 import { SendCard } from "../../../components/SendCard";
 
+//Add Pop-up for successfull transfer.
 export default function () {
     return <div className="text-bold text-center pt-4 w-full">
         <SendCard/>
