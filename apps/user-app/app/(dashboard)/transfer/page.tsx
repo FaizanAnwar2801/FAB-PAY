e1ad0e7be2bc1cd4036d2ca589a9a776dc2d18e3
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-10-23 02:18:57 +0530

    Modified Bank Transfer page & Added Withdrawal.

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -1,9 +1,11 @@
 import prisma from "@repo/db/client";
 import { AddMoney } from "../../../components/AddMoneyCard";
+import { WithdrawMoney } from "../../../components/WithdrawMoneyCard";
 import { BalanceCard } from "../../../components/BalanceCard";
 import { OnRampTransactions } from "../../../components/OnRampTransactions";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../lib/auth";
+import { WithdrawTransactions } from "../../../components/WithdrawTxns";
 
 async function getBalance() {
     const session = await getServerSession(authOptions);
@@ -32,23 +34,40 @@ async function getOnRampTransactions() {
         provider: t.provider
     }))
 }
+async function getWithdrawalTxns() {
+    const session = await getServerSession(authOptions);
+    const txns = await prisma.withdrawals.findMany({
+        where: {
+            userId: Number(session?.user?.id)
+        }
+    });
+    return txns.map(t => ({
+        time: t.startTime,
+        amount: t.amount,
+        status: t.status,
+        provider: t.provider
+    }))
+}
 
 export default async function() {
     const balance = await getBalance();
     const transactions = await getOnRampTransactions();
+    const withdrawals = await getWithdrawalTxns()
 
     return <div className="w-full">
         <div className="text-4xl text-center text-[#6a51a6] pt-4 mb-4 font-bold">
-            Transfer
+            Bank Transfer
         </div>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
-            <div>
+            <div className="space-y-4">
                 <AddMoney />
+                <WithdrawMoney/>
             </div>
             <div>
                 <BalanceCard amount={balance.amount} locked={balance.locked} />
-                <div className="pt-4">
+                <div className="pt-4 space-y-4">
                     <OnRampTransactions transactions={transactions} />
+                    <WithdrawTransactions transactions={withdrawals} />
                 </div>
             </div>
         </div>
