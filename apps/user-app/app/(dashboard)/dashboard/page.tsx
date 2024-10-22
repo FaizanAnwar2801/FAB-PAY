b811e097265613e94ece4960c43a92e567e67643
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-10-23 02:26:05 +0530

    Dashboard Modified..

--- apps/user-app/app/(dashboard)/dashboard/page.tsx
+++ apps/user-app/app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,37 @@
+import prisma from "@repo/db/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../lib/auth";
+import { BalanceCard } from "../../../components/BalanceCard";
 
-export default function () {
-    return <div className="text-bold text-center pt-4">
-        Dashboard
+async function getBalance() {
+    const session = await getServerSession(authOptions);
+    const balance = await prisma.balance.findFirst({
+        where: {
+            userId: Number(session?.user?.id)
+        }
+    });
+    return {
+        amount: balance?.amount || 0,
+        locked: balance?.locked || 0
+    }
+}
+
+
+export default async function () {
+    const session = await getServerSession(authOptions);
+    const balance = await getBalance();
+    const toSentenceCase = (name: string | undefined) => {
+        if (!name) return "";
+        return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+    };
+
+    return <div className="w-full">
+        <div className="font-bold text-left text-2xl p-2 ">
+            Hi, {toSentenceCase(session?.user?.name)}.
+        </div>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-2">
+        <BalanceCard amount={balance.amount} locked={balance.locked} />
+        </div>
     </div>
+    
 }
