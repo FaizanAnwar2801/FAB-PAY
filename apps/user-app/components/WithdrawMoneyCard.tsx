7a68b2df24ee9ae089d70aad977e45e327c6fa9b
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-10-23 02:06:06 +0530

    Added card to implement withdrawal of money.

---
+++ apps/user-app/components/WithdrawMoneyCard.tsx
@@ -0,0 +1,46 @@
+"use client"
+import { Button } from "@repo/ui/button";
+import { Card } from "@repo/ui/card";
+import { Select } from "@repo/ui/select";
+import { useState } from "react";
+import { TextInput } from "@repo/ui/textinput";
+import { createWithdrawals } from "../app/lib/actions/withdrawals";
+
+const SUPPORTED_BANKS = [{
+    name: "HDFC Bank",
+    redirectUrl: "https://netbanking.hdfcbank.com"
+}, {
+    name: "Axis Bank",
+    redirectUrl: "https://www.axisbank.com/"
+}];
+
+export const WithdrawMoney = () => {
+    const [redirectUrl, setRedirectUrl] = useState(SUPPORTED_BANKS[0]?.redirectUrl);
+    const [provider, setProvider] = useState(SUPPORTED_BANKS[0]?.name || "");
+    const [value, setValue] = useState(0);
+    return <Card title="Withdraw Money">
+    <div className="w-full">
+        <TextInput label={"Amount"} placeholder={"Amount"} onChange={(value) => {
+            setValue(Number(value))
+        }} />
+        <div className="py-4 text-left">
+            Bank
+        </div>
+        <Select onSelect={(value) => {
+            setRedirectUrl(SUPPORTED_BANKS.find(x => x.name === value)?.redirectUrl || "");
+            setProvider(SUPPORTED_BANKS.find(x => x.name === value)?.name || "");
+        }} options={SUPPORTED_BANKS.map(x => ({
+            key: x.name,
+            value: x.name
+        }))} />
+        <div className="flex justify-center pt-4">
+            <Button onClick={async () => {
+                await createWithdrawals(provider, value)
+                window.location.href = redirectUrl || "";
+            }}>
+            Withdraw
+            </Button>
+        </div>
+    </div>
+</Card>
+}
