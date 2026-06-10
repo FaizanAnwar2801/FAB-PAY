0bf2f3ba61f738012eb632e57088eeb0e57809be
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2026-06-10 22:27:17 +0530

    Migrated to prisma 7 (tested with old schema and local postgres DB)

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,15 +1,26 @@
-import { PrismaClient } from '@prisma/client'
+import {PrismaClient} from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const prismaClientSingleton = () => {
-    return new PrismaClient()
-}
+  const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
+  });
+
+  return new PrismaClient({
+    adapter,
+  });
+};
 
 declare global {
-    var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
+
+  var prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined;
 }
 
-const prisma: ReturnType<typeof prismaClientSingleton> = globalThis.prismaGlobal ?? prismaClientSingleton()
+const prisma =
+  globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default prisma
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prismaGlobal = prisma;
+}
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+export default prisma;
--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -1,85 +1,94 @@
-import { PrismaClient } from '@prisma/client'
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 import bcrypt from "bcrypt";
-const prisma = new PrismaClient()
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
 
 async function main() {
-    const alice = await prisma.user.upsert({
-        where: { number: '1111111111' },
-        update: {},
+  const alice = await prisma.user.upsert({
+    where: { number: "1111111111" },
+    update: {},
+    create: {
+      number: "1111111111",
+      password: await bcrypt.hash("alice", 10),
+      name: "alice",
+      Balance: {
         create: {
-            number: '1111111111',
-            password: await bcrypt.hash('alice', 10),
-            name: 'alice',
-            Balance: {
-                create: {
-                    amount: 20000,
-                    locked: 0
-                }
-            },
-            OnRampTransaction: {
-                create: {
-                    startTime: new Date(),
-                    status: "Success",
-                    amount: 70000,
-                    token: "token__1",
-                    provider: "HDFC Bank",
-                }
-            },
-            Withdrawals: {
-                create: {
-                    startTime: new Date(),
-                    status: "Success",
-                    amount: 50000,
-                    token: "token__1",
-                    provider: "HDFC Bank",
-                }
-            }
-        }
-    });
-
-    const bob = await prisma.user.upsert({
-        where: { number: '2222222222' },
-        update: {},
+          amount: 20000,
+          locked: 0,
+        },
+      },
+      OnRampTransaction: {
         create: {
-            number: '2222222222',
-            password: await bcrypt.hash('bob', 10),
-            name: 'bob',
-            Balance: {
-                create: {
-                    amount: 20000,
-                    locked: 0
-                }
-            },
-            OnRampTransaction: {
-                create: {
-                    startTime: new Date(),
-                    status: "Failure",
-                    amount: 45000,
-                    token: "token__2",
-                    provider: "HDFC Bank",
-                }
-            },
-            Withdrawals: {
-                create: {
-                    startTime: new Date(),
-                    status: "Failure",
-                    amount: 25000,
-                    token: "token__2",
-                    provider: "HDFC Bank",
-                }
-            }
-        }
-    });
+          startTime: new Date(),
+          status: "Success",
+          amount: 70000,
+          token: "token__1",
+          provider: "HDFC Bank",
+        },
+      },
+      Withdrawals: {
+        create: {
+          startTime: new Date(),
+          status: "Success",
+          amount: 50000,
+          token: "withdraw_token__1",
+          provider: "HDFC Bank",
+        },
+      },
+    },
+  });
 
-    console.log({ alice, bob });
+  const bob = await prisma.user.upsert({
+    where: { number: "2222222222" },
+    update: {},
+    create: {
+      number: "2222222222",
+      password: await bcrypt.hash("bob", 10),
+      name: "bob",
+      Balance: {
+        create: {
+          amount: 20000,
+          locked: 0,
+        },
+      },
+      OnRampTransaction: {
+        create: {
+          startTime: new Date(),
+          status: "Failure",
+          amount: 45000,
+          token: "token__2",
+          provider: "HDFC Bank",
+        },
+      },
+      Withdrawals: {
+        create: {
+          startTime: new Date(),
+          status: "Failure",
+          amount: 25000,
+          token: "withdraw_token__2",
+          provider: "HDFC Bank",
+        },
+      },
+    },
+  });
+
+  console.log({ alice, bob });
 }
 
 main()
-    .then(async () => {
-        await prisma.$disconnect();
-    })
-    .catch(async (e) => {
-        console.error(e);
-        await prisma.$disconnect();
-        process.exit(1);
-    });
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
